b99ead6c4be3c7e8550ff11746f5ee0d70a4eab0
Monis <monissms16@gmail.com>
2025-05-20 10:07:05 +0530

    Add props and tailwind

---
+++ 03_propsAndTailwind/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Card from './componenets/Card.jsx'
+
+function App() {
+
+
+  return (
+   <>
+   <h1 className='bg-red-800 p-4 rounded-xl '>Helllooo</h1>
+  <Card username='Monis' />
+  <Card username='Death' />
+  <Card />
+   </>
+  )
+}
+
+export default App
